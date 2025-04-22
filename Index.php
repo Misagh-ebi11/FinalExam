7f9238f618e76397c9ce7b4af0d8d3c43c891e64
Misagh-ebi11 <misaghebrahimi918@gmail.com>
2025-04-23 01:20:11 +0330

    Basic structure design

---
+++ Index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body{
+            text-align: center;
+            background-image: url("Pictures/Second/vecteezy_abstract-background-design-background-texture-design-with_18752867-1.jpg");
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+        #Content{
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+        }
+        #Header , .Title{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+        .Title{
+            padding: 10px;
+        }
+        .Button{
+            width: 400px;
+            height: 300px;
+            background-color: rgb(120, 247, 177);
+            margin-left: 0.1% ;
+            border-radius: 50px;
+        }
+    </style>
+</head>
+<body>
+    <div id="Header">
+        <h1>Published by <br> Misagh ebrahimi</h1>
+    </div>
+    <div id="Content">
+        <div>
+            <h2 class="Title">Perfect numbers</h2>
+            <input type="button" class="Button">
+        </div>
+        <div>
+            <h2 class="Title">Perfect numbers</h2>
+            <input type="button" class="Button">
+        </div>
+        <div>
+            <h2 class="Title">Perfect numbers</h2>
+            <input type="button" class="Button">
+        </div>
+    </div>
+</body>
+</html>
